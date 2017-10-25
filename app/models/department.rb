448950842cbc2492554e1967f01697ae423f86b6
Ben Rooney <ben@benrooney.com>
2017-10-25 10:41:15 +0100

    sort out edit departments

--- app/models/department.rb
+++ app/models/department.rb
@@ -1,6 +1,6 @@
 class Department < ApplicationRecord
   validates :name, presence: true
-  validates :name, uniqueness:  {scope: :school_id, message: "Departments must be unique"}
+  validates :name, uniqueness:  {scope: :school_id, message: "of Department must be unique"}
 
   belongs_to :school
 end

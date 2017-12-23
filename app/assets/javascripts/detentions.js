c6835dbb721ad699a8588e91eec11d0b4244fdb0
Ben Rooney <ben@benrooney.com>
2017-12-23 02:59:25 +0000

    getting more ajax to work on new student

--- app/assets/javascripts/detentions.js
+++ app/assets/javascripts/detentions.js
@@ -2,7 +2,7 @@
 
 $(document).on('turbolinks:load', function() {
 
-
+  $("#new_student").hide()
 
 
   $( "#detention_student_name" ).autocomplete({
@@ -14,4 +14,26 @@ $(document).on('turbolinks:load', function() {
           return false;
         }
     });
+
+  $("#new_student_link").click(function (){
+    $("#new_student").toggle()
+    return false;
+  })
+
+
+
+  $("#new_student").submit(function (event){
+      alert('the action is: ' + $("#new_student").attr('action'));
+  //    $.ajax(
+  //      url: url,
+  //      type: 'POST',
+  //      dataType: 'script',
+  //      function(data, status){
+  //       alert("Data: " + data + "\nStatus: " + status);
+  //    });
+      return false
+  });
+
+
+
 });

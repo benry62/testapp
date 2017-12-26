a0beb7d24c7eceac5b82e465742bfbc84d0ebb6e
Ben Rooney <ben@benrooney.com>
2017-12-26 21:41:59 +0000

    trying out grouped_collection_select

--- app/models/form.rb
+++ app/models/form.rb
@@ -6,4 +6,17 @@ class Form < ApplicationRecord
 
   belongs_to :school
   has_many :students
+
+  def self.grouped_by_year(year, school_id)
+    groups = []
+    for year in 7..13
+      Form.where(year: year, school_id: school_id).distinct(:name).each_with_index do |name, index|
+        groups << [name, Form.where(year: year, name: name)]
+      end
+    end
+
+    return groups
+  end
+
+
 end

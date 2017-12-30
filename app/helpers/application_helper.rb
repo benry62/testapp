3be299e9ec4bb7f076e3024ee9a84e0d1215a84b
Ben Rooney <ben@benrooney.com>
2017-12-30 13:40:18 +0000

    Trying to get rid of superfluous classes and replace with flat files

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,4 +3,12 @@ module ApplicationHelper
   def human_boolean(boolean)
     boolean ? 'Yes' : 'No'
   end
+
+  def school_name
+    f = YAML.load_file("config/school_config.yml")
+    f["name"]
+  end
+
+
+
 end
